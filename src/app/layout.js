b1d2971b9a853e2657b1ba9db15e12ba0596ddd5
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-14 00:12:28 +0600

    change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outFitFont = Outfit({
+  
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Pixgen",
