38725774367d59b9fda8e7ccf9f457130ddf6e12
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-14 02:52:15 +0600

    install react hot toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const outFitFont = Outfit({
   
@@ -20,15 +21,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${outFitFont.className}  h-full antialiased`}
-      
-    >
+    <html lang="en" className={`${outFitFont.className}  h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
-        <main >{children}</main>
+        <main>{children}</main>
         <Footer></Footer>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              borderRadius: "12px",
+              background: "#18181b",
+              color: "#fff",
+            },
+          }}
+        />
       </body>
     </html>
   );
