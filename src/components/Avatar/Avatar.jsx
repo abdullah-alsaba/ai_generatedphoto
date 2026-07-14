a171cd8c54a8791f4de3abfd33cf6dbe9c26d009
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-14 20:20:38 +0600

    feat: added sign out feature

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,6 +1,13 @@
-import { Avatar } from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
+import toast from "react-hot-toast";
 
 export default function UserAvatar({ user }) {
+  const handelSignOut = async () => {
+    await authClient.signOut();
+    toast.success("Sign Out Successful")
+  }
+
   return (
     <div className="flex gap-4">
       <Avatar>
@@ -11,6 +18,7 @@ export default function UserAvatar({ user }) {
         />
               <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
       </Avatar>
+      <Button onClick={()=>handelSignOut()} variant="outline" className="text-black" >Sign Out</Button>
     </div>
   );
 }
